2799b968d4aa0303e2a0d713a1cf1247ed44bae2
gomudayya <gomudayya@gmail.com>
2024-08-08 12:11:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/ticketland/ticketland/TicketlandApplication.java
@@ -0,0 +1,13 @@
+package com.ticketland.ticketland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketlandApplication.class, args);
+	}
+
+}
