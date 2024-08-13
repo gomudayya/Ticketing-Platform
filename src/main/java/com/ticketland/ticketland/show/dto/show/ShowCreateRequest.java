df6905e935e6e5033f726ea38def07477c750e25
gomudayya <gomudayya@gmail.com>
2024-08-13 10:30:31 +0900

    refactor : dto 제약조건 추가

--- src/main/java/com/ticketland/ticketland/show/dto/show/ShowCreateRequest.java
+++ src/main/java/com/ticketland/ticketland/show/dto/show/ShowCreateRequest.java
@@ -4,6 +4,9 @@ import com.ticketland.ticketland.show.domain.Genre;
 import com.ticketland.ticketland.show.domain.Show;
 import com.ticketland.ticketland.show.domain.Venue;
 import com.ticketland.ticketland.show.dto.TicketPriceDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,14 +20,32 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class ShowCreateRequest {
+
+    @NotNull
     private Long venueId;
+
+    @NotNull
     private Long genreId;
+
+    @NotBlank
     private String performer;
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String descriptionImage; // 후.. 원래는 이미지 파일로 올려야 하는데 일단은... 요렇게하자.
+
+    @NotBlank
     private LocalDateTime ticketingTime;
+
+    @NotBlank
     private LocalDateTime startDate;
+
+    @NotNull
     private Integer duration;
+
+    @NotEmpty
     private List<TicketPriceDto> ticketPrices;
 
     public Show toEntity(Genre genre, Venue venue) {
--- src/main/java/com/ticketland/ticketland/show/dto/venue/VenueCreateRequest.java
+++ src/main/java/com/ticketland/ticketland/show/dto/venue/VenueCreateRequest.java
@@ -2,6 +2,7 @@ package com.ticketland.ticketland.show.dto.venue;
 
 import com.ticketland.ticketland.show.domain.Venue;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,6 +21,7 @@ public class VenueCreateRequest {
     @NotBlank
     private String seatLayoutData;
 
+    @NotEmpty
     private Map<String, Integer> seatCountDetails; // <Section, 해당 Section 의 좌석갯수>
 
     public Venue toEntity() {
