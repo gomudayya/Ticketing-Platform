d830d7ddfe6a2ab0b2af40e0de70809b0a906686
gomudayya <gomudayya@gmail.com>
2024-08-27 13:44:18 +0900

    refactor : 회원가입 입력 포맷에 따른 예외메시지 추가

--- user-service/src/main/java/com/example/userservice/user/dto/JoinRequest.java
+++ user-service/src/main/java/com/example/userservice/user/dto/JoinRequest.java
@@ -1,6 +1,5 @@
 package com.example.userservice.user.dto;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -13,18 +12,17 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JoinRequest {
-    @NotBlank
-    @Email
+
+    @NotBlank(message = "Email은 비어 있을 수 없습니다.")
+    @Email(message = "유효한 이메일 주소를 입력해주세요.")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호는 비어 있을 수 없습니다.")
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "이름은 비어 있을 수 없습니다.")
     private String name;
 
-    @NotBlank
+    @NotBlank(message = "전화번호는 비어 있을 수 없습니다.")
     private String phoneNumber;
-
-
 }
